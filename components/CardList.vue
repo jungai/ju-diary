8d3505e7e37533aa15b0c7ecd2372b729e690718
juuuu🐳 <juworaphol125@gmail.com>
2020-07-18 20:26:34 +0700

    feat: add CardList Component

---
+++ components/CardList.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="card-list">
+    <slot />
+  </div>
+</template>
+
+<script>
+import {} from 'nuxt-composition-api'
+export default {
+  name: 'CardList',
+  setup() {
+    return {}
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.card-list {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  max-width: 960px;
+  width: 100%;
+  margin: 0 auto;
+  padding: 0.8rem;
+}
+</style>
