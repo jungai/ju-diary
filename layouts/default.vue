dd5983336ad4ea5dc1fa9f0d38e9d3831c87d804
juuuu🐳 <juworaphol125@gmail.com>
2020-07-18 20:25:56 +0700

    refactor(layout): set font family and remove prefix in default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,33 +1,25 @@
 <template>
-  <div>
+  <div class="home">
     <Header />
     <nuxt />
-    <Footer />
   </div>
 </template>
 
 <script>
 import Header from '@/components/Header'
-import Footer from '@/components/Footer'
 
 export default {
   components: {
     Header,
-    Footer,
   },
 }
 </script>
 
 <style>
 html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-family: 'Lato', sans-serif;
   font-size: 16px;
   word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
   box-sizing: border-box;
 }
 
