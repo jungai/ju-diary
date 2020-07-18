c495269a37653df0c26dca2c7d8e189c7b797d80
juuuu🐳 <juworaphol125@gmail.com>
2020-07-18 20:27:38 +0700

    refactor: landing page

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,21 @@
 <template>
-  <main class="container">
-    <div class="pages">
-      <nuxt-link
-        v-for="(post, index) in posts"
-        :key="`${index}${post.title}`"
-        :to="post.slug"
-        class="box"
-      >
-        <h2>{{ post.title }}</h2>
-        <p>{{ post.description }}</p>
-      </nuxt-link>
-    </div>
-  </main>
+  <CardList>
+    <Card
+      v-for="post in posts"
+      :key="post.slug"
+      :title="post.title"
+      :description="post.description"
+    />
+  </CardList>
 </template>
 
 <script>
 import { useAsync } from 'nuxt-composition-api'
+import CardList from '@/components/CardList'
+import Card from '@/components/Card'
 
 export default {
+  components: { CardList, Card },
   setup(_, ctx) {
     const posts = useAsync(async () => await ctx.root.$content('posts').fetch())
 
@@ -26,32 +24,4 @@ export default {
 }
 </script>
 
-<style scoped>
-main {
-  display: flex;
-  padding: 2.5em;
-  justify-content: center;
-  align-items: center;
-}
-
-aside {
-  width: 500px;
-  align-self: start;
-  padding: 1em;
-  margin-right: 1em;
-  border: 1px solid lightgrey;
-}
-
-.pages {
-  max-width: 1024px;
-  display: flex;
-}
-
-.nuxt-link {
-  width: 500px;
-  margin: 20px;
-  background-color: tomato;
-  display: flex;
-  flex-direction: column;
-}
-</style>
+<style></style>
