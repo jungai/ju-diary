8492a0487dc20398a980af1c48c7e405648d6e2f
jungai <juworaphol125@gmail.com>
2020-07-29 21:15:06 +0700

    add blog layout

---
+++ layouts/blog.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <nuxt />
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+html {
+  font-family: 'Lato', sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  box-sizing: border-box;
+}
+
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+  margin: 0;
+}
+
+.home {
+  margin: 0 auto;
+  max-width: 960px;
+  width: 100%;
+  position: relative;
+}
+</style>
