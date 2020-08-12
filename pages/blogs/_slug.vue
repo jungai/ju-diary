4ed7f8a93b96e490f5ef1d6b601c399c10267c6d
jungai <juworaphol125@gmail.com>
2020-08-12 17:24:09 +0700

    custom nuxt-content styling

--- pages/blogs/_slug.vue
+++ pages/blogs/_slug.vue
@@ -1,5 +1,5 @@
 <template>
-  <article>
+  <article class="content">
     <h1>{{ doc[0].title }}</h1>
     <nuxt-content :document="doc[0]" />
   </article>
@@ -22,4 +22,26 @@ export default {
 }
 </script>
 
-<style></style>
+<style lang="scss">
+.content {
+  padding: 1em;
+
+  > h1 {
+    text-align: center;
+    font-size: 2em;
+    margin-bottom: 0.8em;
+    border-bottom: tomato 4px solid;
+  }
+
+  .nuxt-content {
+    font-size: 1.2em;
+  }
+
+  .nuxt-content .start::first-letter {
+    float: left;
+    font-size: 3em;
+    margin: 0.2em;
+    color: tomato;
+  }
+}
+</style>
